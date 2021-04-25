a14081cb472f1f210363b510268c004568ddd478
pedkhod <pkhoddami@gmail.com>
2021-04-25 15:12:35 +0430

    frim

---
+++ Snake.py
@@ -0,0 +1,32 @@
+#Snake
+import math
+import random
+import pygame
+import tkinter as tk
+from Tkinter import messagebox
+#classes
+class cube(object):
+    row=0
+    w= 0
+    def __init__(self,start,dirnx=1,dirny=0,color=(255,0,0)):
+        pass
+    def move(self,dirnx,dirny):
+        pass
+    def draw(self,surface,eyes=False):
+        pass
+class snake(object):
+    def __init__(self,color,pos):
+        pass
+    def move(self):
+        pass
+    def reset(self,pos):
+        pass
+#function
+def drawGrid(w,rows,surface):
+    pass
+def randomSnakes(row,item):
+    pass
+def massage_box(subject,content):
+    pass
+def main():
+    pass
